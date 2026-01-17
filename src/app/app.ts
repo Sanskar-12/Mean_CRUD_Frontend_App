9bb4f310fdcb59e1716727c3308e410d6f38467e
Sanskar-12 <sanskarv2004@gmail.com>
2026-01-17 16:51:31 +0530

    Feature: Made use of FormModule for validations and Common module for conditions

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('student-portal');
+
+  // intialising form
+  studentForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.studentForm = fb.group({
+      name: ['', Validators.required],
+      age: ['', [Validators.required, Validators.min(1)]],
+      email: ['', [Validators.required, Validators.email]],
+      studentClass: ['', Validators.required],
+      address: ['', Validators.required],
+      phone: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+    });
+  }
+
+  onSubmit() {}
 }
